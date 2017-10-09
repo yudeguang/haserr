55c6540d4db51af8c88583335c0a0e8cfb1a5427
yudeguang <84558040@qq.com>
2017-10-09 08:48:01 +0800

    Signed-off-by: yudeguang

--- haserr.go
+++ haserr.go
@@ -7,12 +7,14 @@ import (
 	"strings"
 )
 
-// 判断有无错误,并返回true或者false
+// 判断有无错误,有错误返回true，无错误返回false,一般情况下不会调用此函数，建议用 err!=nil 代替
 func HasErr(err error) bool {
 	if err != nil {
 		return true
+	} else {
+		return false
 	}
-	return false
+	//return err!=nil //这样写不好理解
 }
 
 // 判断有无错误,并返回true或者false,有错误时打印错误
@@ -22,8 +24,9 @@ func Println(err error) bool {
 		file = file[strings.LastIndex(file, `/`)+1:]
 		log.Printf("%v,第%v行,错误类型:%v", file, line, err)
 		return true
+	} else {
+		return false
 	}
-	return false
 }
 
 //判断有无错误,并返回true或者false,有错误时调用log.Fatal退出
@@ -33,8 +36,9 @@ func Fatal(err error) bool {
 		file = file[strings.LastIndex(file, `/`)+1:]
 		log.Fatalf("%v,第%v行,错误类型:%v", file, line, err)
 		return true
+	} else {
+		return false
 	}
-	return false
 }
 
 //判断有无错误,并返回true或者false,有错误时调用panic退出
@@ -44,6 +48,7 @@ func Panic(err error) bool {
 		file = file[strings.LastIndex(file, `/`)+1:]
 		panic(fmt.Sprintf("%v,第%v行,错误类型:%v", file, line, err))
 		return true
+	} else {
+		return false
 	}
-	return false
 }
